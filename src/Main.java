9decf4a320a4ed7c72d66d22e57d78a8292cdd8d
jgreenwell <jsgreenwell83@gmail.com>
2026-05-18 10:47:58 -0500

    fixed main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import Games.ConnectFour;
 import Games.RockPaperScissors;
 import Games.TicTacToe;
 import java.util.Scanner;
-import java.util.logging.FileHandler;
 
 
 // Menu for Game Selection
@@ -11,9 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        boolean running = true;
-
-        while (running) {
+        while (true) {
             System.out.println("=== Pick Any Game ===");
             System.out.println("1. Connect Four");
             System.out.println("2. Rock Paper Scissors");
@@ -27,43 +24,44 @@ public class Main {
             int choice = Integer.parseInt(scanner.nextLine().trim());
 
             try {
-              switch (choice) {
-                  case 1:
-                      ConnectFour connectFour = new ConnectFour(scanner);
-                      connectFour.play();
+                switch (choice) {
+                    case 1:
+                        ConnectFour connectFour = new ConnectFour(scanner);
+                        connectFour.play();
 
-                      System.out.print("Want to Play Again? (y/n): ");
-                      String again = scanner.nextLine().trim().toLowerCase();
+                        System.out.print("Want to Play Again? (y/n): ");
+                        String again = scanner.nextLine().trim().toLowerCase();
 
-                      if (again.equals("y")) {
-                          connectFour = new ConnectFour(scanner); // reset game
-                          connectFour.play();
-                      }
-                      break;
+                        if (again.equals("y")) {
+                            connectFour = new ConnectFour(scanner); // reset game
+                            connectFour.play();
+                        }
+                        break;
 
-                  case 2:
-                      RockPaperScissors rps = new RockPaperScissors(scanner);
-                      rps.play();
-                      break;
+                    case 2:
+                        RockPaperScissors rps = new RockPaperScissors(scanner);
+                        rps.play();
+                        break;
 
-                  case 3:
-                      TicTacToe ticTacToe = new TicTacToe(scanner);
-                      ticTacToe.play();
+                    case 3:
+                        TicTacToe ticTacToe = new TicTacToe(scanner);
+                        ticTacToe.play();
 
-                      System.out.print("Play Tic Tac Toe again? Press y to Continue Game.");
-                      String again3 = scanner.nextLine().trim().toLowerCase();
+                        System.out.print("Play Tic Tac Toe again? Press y to Continue Game.");
+                        String again3 = scanner.nextLine().trim().toLowerCase();
 
-                      if (again3.equals("y")) {
-                          ticTacToe = new TicTacToe(scanner); // resets the game
-                          ticTacToe.play();
-                      }
-                      break;
+                        if (again3.equals("y")) {
+                            ticTacToe = new TicTacToe(scanner); // resets the game
+                            ticTacToe.play();
+                        }
+                        break;
 
                     case 4:
-                       new WarGame().play();
-                       break;
+                        new WarGame().play();
+                        break;
 
                     case 5:
+                        WordScrambleGame scramble = new WordScrambleGame();
                         scramble.play();
                         break;
 
@@ -72,21 +70,19 @@ public class Main {
                         break;
 
                     case 7:
+                        System.out.println();
                         System.out.println("Goodbye!");
-                        running = false;
+                        scanner.close();
                         return;
 
                     default:
                         System.out.println("Invalid choice.");
-              }
-           } catch (Exception e) {
+                }
+            } catch (Exception e) {
                 System.out.println("Please enter a number.");
                 // Nice call here
             }
-
-            System.out.println();
-            System.out.println("Goodbye!");
-            scanner.close();
+        }
     }
 }
 
